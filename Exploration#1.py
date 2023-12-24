26a2c7b9188183bc5effa8215595226509694a3e
HamzaA598 <hamzaemad598@gmail.com>
2023-12-24 14:12:14 +0200

    add: bar chart for source vs target

--- Exploration#1.py
+++ Exploration#1.py
@@ -25,7 +25,7 @@ plt.show()
 # plt.figure(figsize=(8, 6))
 # target_counts.plot(kind='bar', color='skyblue')
 # plt.title('Frequency of Each Category in the "Target" Column')
-# plt.xlabel('Target')
+# plt.xlabel('Target')  
 # plt.ylabel('Frequency')
 # plt.show()
 
@@ -33,4 +33,23 @@ plt.show()
 plt.figure(figsize=(8, 8))
 plt.pie(target_counts, labels=target_counts.index, autopct='%1.1f%%', startangle=90, colors=['lightcoral', 'lightgreen', 'lightblue'])
 plt.title('Distribution of Categories in the "Target" Column')
+plt.show()
+
+#bar chart for sources and target
+
+# Group by 'Source' and 'Target' columns and count occurrences
+grouped_data = df.groupby(['Source', 'Target']).size().unstack()
+
+# Plot the bar chart
+grouped_data.plot(kind='bar', stacked=True, color=['lightgreen', 'lightblue'])
+
+# Set labels and title
+plt.xlabel('Source')
+plt.ylabel('Count')
+plt.title('Bar Chart of Source vs Target')
+
+# Display the legend
+plt.legend(title='Target', labels=['0', '1'])
+
+# Show the plot
 plt.show()
