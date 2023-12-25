9343770691285fec0782bd75c479c836612cfd11
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-12-25 15:27:58 +0200

    removed useless print statements

--- ANN.py
+++ ANN.py
@@ -38,14 +38,15 @@ y = df.iloc[:, -1]
 vectorizer = TfidfVectorizer()
 sentence_embeddings = vectorizer.fit_transform(X)
 
-print("\nTf-idf Feature Names:")
-print(vectorizer.get_feature_names_out())
+# print("\nTf-idf Feature Names:")
+# print(vectorizer.get_feature_names_out())
 
-print("\nTf-idf Matrix:")
-print(sentence_embeddings.toarray())
+# print("\nTf-idf Matrix:")
+# print(sentence_embeddings.toarray())
 
 
 X_train, X_test, y_train, y_test = train_test_split(sentence_embeddings.toarray(), y, test_size=0.2, random_state=42)
+
 def build_model(neurons=16, learning_rate=0.01):
   model = Sequential()
   model.add(layers.Dense(units=neurons, activation='relu', input_dim=X_train.shape[1]))
@@ -59,12 +60,12 @@ def build_model(neurons=16, learning_rate=0.01):
 
 model = KerasClassifier(build_fn=build_model,neurons = [16, 32, 64, 128], learning_rate = [0.001, 0.01, 0.1], batch_size=[16, 32, 64], verbose=0)
 
-print(model.get_params().keys())
 param_grid = {
     'neurons': [16, 32, 64, 128],
     'learning_rate': [0.001, 0.01, 0.1],
     'batch_size': [16, 32, 64]
 }
+
 grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
 grid_result = grid.fit(X_train, y_train)
 
