596f365947f66fbfc26ca0933cb66fef74549128
HamzaA598 <hamzaemad598@gmail.com>
2023-12-26 14:13:28 +0200

    merge: Exploration and visualization

--- Exploration.py
+++ Exploration.py
@@ -37,19 +37,34 @@ plt.show()
 
 #bar chart for sources and target
 
-# Group by 'Source' and 'Target' columns and count occurrences
+# Group by 'source' and 'target' and get the count
 grouped_data = df.groupby(['Source', 'Target']).size().unstack()
 
-# Plot the bar chart
-grouped_data.plot(kind='bar', stacked=True, color=['lightgreen', 'lightblue'])
+# Plotting the bar chart
+fig, ax = plt.subplots()
+width = 0.25  # the width of the bars
 
-# Set labels and title
-plt.xlabel('Source')
-plt.ylabel('Count')
-plt.title('Bar Chart of Source vs Target')
+sources = grouped_data.index
+ind = range(len(sources))
 
-# Display the legend
-plt.legend(title='Target', labels=['0', '1'])
+bar_0 = ax.bar(ind, grouped_data[0], width, label='0')
+bar_1 = ax.bar([i + width for i in ind], grouped_data[1], width, label='1')
 
-# Show the plot
+# Adding labels and title
+ax.set_xlabel('Source')
+ax.set_ylabel('Count')
+ax.set_title('Count of Positive and Negative by Source')
+ax.set_xticks([i + width/2 for i in ind])
+ax.set_xticklabels(sources)
+ax.legend()
+
+for bar in bar_0:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
+
+for bar in bar_1:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
+
+
 plt.show()
